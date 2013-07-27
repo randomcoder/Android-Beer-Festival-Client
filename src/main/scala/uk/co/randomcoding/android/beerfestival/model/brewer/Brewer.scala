22c5b524b9aa393a7bcaf19d01adebc6fc52ee12
RandomCoder <randomcoder@randomcoding.co.uk>
2013-07-27 15:41:25 +0100

    Added brewer class (including convertion form JSON)
    Also added some converters in a new object to implicitly manage some of
    the common changes used in the JSOn conversions.
    
    re: #1

---
+++ src/main/scala/uk/co/randomcoding/android/beerfestival/model/brewer/Brewer.scala
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) 2013 - RandomCoder <randomcoder@randomcoding.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *    RandomCoder <randomcoder@randomcoding.co.uk> - initial API and implementation and/or initial documentation
+ */
+package uk.co.randomcoding.android.beerfestival.model.brewer
+
+import scala.util.parsing.json.JSON
+
+import uk.co.randomcoding.android.beerfestival.util.Convertors._
+
+/**
+ * @constructor Create a new instance of a Brewer
+ * @param name The brewer's namer
+ * @param location Where the brewer is located
+ * @param drinkUids The `uids` of all the drinks brewed by this brewer
+ *
+ * @author RandomCoder
+ */
+case class Brewer(name: String, location: String, drinkUids: Set[String])
+
+object Brewer {
+  private[this] val TAG = "Brewer Parser"
+
+  def fromJson(jsonData: String): Seq[Brewer] = JSON.parseFull(jsonData) match {
+    case Some(data) => data match {
+      case brewersData: List[_] => convertBrewers(brewersData).distinct
+      case brewerData: Map[_, _] => Seq(brewerFromJson(brewerData))
+      case _ => Nil
+    }
+    case failedParse => Nil
+  }
+
+  private[this] def brewerFromJson(brewerJson: Map[String, Any]): Brewer = {
+    val name = brewerJson("name").toString
+    val location = brewerJson("location").toString
+    val drinkUids = brewerJson("drinkUids") match {
+      case uids: List[_] => uids.map(_.toString).toSet
+      case _ => Set.empty[String]
+    }
+
+    Brewer(name, location, drinkUids)
+  }
+
+  private[this] def convertBrewers(brewerJsonData: List[_]): List[Brewer] = {
+    val brewers = brewerJsonData.map(_ match {
+      case brewerDataMap: Map[_, _] => Some(brewerFromJson(brewerDataMap))
+      case _ => None
+    })
+
+    brewers
+  }
+}
---
+++ src/main/scala/uk/co/randomcoding/android/beerfestival/util/Convertors.scala
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2013 - RandomCoder <randomcoder@randomcoding.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *    RandomCoder <randomcoder@randomcoding.co.uk> - initial API and implementation and/or initial documentation
+ */
+package uk.co.randomcoding.android.beerfestival.util
+
+/**
+ * Brief description of Convertors
+ *
+ * @author RandomCoder
+ *
+ */
+object Convertors {
+
+  implicit def anyAnyMapToStringAny(in: Map[_, _]): Map[String, _] = in map { case (k, v) => (k.toString, v) }
+
+  implicit def optionListToDefinedList[A](in: List[Option[A]]): List[A] = in.filter(_.isDefined).map(_.get)
+}
