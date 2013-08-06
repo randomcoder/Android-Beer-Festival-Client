070b05551c078af9da62b208aebf2f7eac6f434f
RandomCoder <randomcoder@randomcoding.co.uk>
2013-08-06 22:59:07 +0100

    Moved extra keys into new object and remove search drink content and layout
    re: #7

---
+++ src/main/scala/uk/co/randomcoding/android/beerfestival/util/IntentExtras.scala
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2013 - RandomCoder <randomcoder@randomcoding.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *    RandomCoder <randomcoder@randomcoding.co.uk> - initial API and implementation and/or initial documentation
+ */
+package uk.co.randomcoding.android.beerfestival.util
+
+/**
+ * Values to use as Keys for passing data in Intent extra bundles
+ *
+ * @author RandomCoder
+ *
+ */
+object IntentExtras {
+  private def extra(key: String): String = s"uk.co.randomcoding.android.drinkfinder.$key"
+
+  final val NAME_SEARCH_EXTRA = extra("NameSearch")
+  final val DESCRIPTION_SEARCH_EXTRA = extra("DescriptionSearch")
+  final val FESTIVAL_ID_EXTRA = extra("FestivalId")
+}
